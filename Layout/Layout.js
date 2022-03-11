c7762ab2653888c851236f0a51acccbb0db29288
Joe Grant <jmgrant1995@gmail.com>
2022-03-11 08:28:06 +0800

    add layout component

---
+++ Layout/Layout.js
@@ -0,0 +1,13 @@
+import Header from '../components/header'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default Layout
